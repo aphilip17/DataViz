f56ec5babc0398a7105a574de6dd74050771171e
aurore <aurore.philip@gmail.com>
2020-05-06 17:19:27 +0200

    Add lib Turf.js to calculate centroid of polygons and display circles

--- src/components/MyMap.vue
+++ src/components/MyMap.vue
@@ -1,12 +1,15 @@
 <script>
 import { latLng } from 'leaflet';
-import { LMap, LTileLayer } from 'vue2-leaflet';
+import { LMap, LTileLayer, LFeatureGroup, LCircle } from 'vue2-leaflet';
+import Centroid from '@turf/centroid';
 
 export default {
     name: 'my-map',
     components: {
         LMap,
         LTileLayer,
+        LFeatureGroup,
+        LCircle,
     },
     data() {
         return {
@@ -15,31 +18,48 @@ export default {
             url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
             subdomains: 'abcd',
-            maxZoom: 19
+            maxZoom: 19,
+            depts: {},
+            circles: []
         }
     },
 
     created() {
-        this.getDeptsGeojson();
+        this.displayCircles();
+    },
+
+    mounted() {
+        this.$nextTick(() => {
+            console.log(this.$refs.featureGroup);
+        });
     },
 
     methods: {
         async getDeptsGeojson() {
             try {
                 const response = await fetch('http://localhost:8080/depts.json');
-                const depts = await response.json();
-                console.log(depts);
+                this.depts = await response.json();
+                console.log(this.depts);
             } catch (error) {
                 console.log(error);
             }
 
+        },
+
+        async displayCircles() {
+            await this.getDeptsGeojson();
+            this.circles = this.depts.features.map((dpt) => {
+                return Centroid(dpt, dpt.properties);
+            });
+
+            console.log(this.circles);
         }
     }
 }
 </script>
 
 <template>
-    <div style="height: 500px; width: 50%">
+    <div style="height: 500px; width: 100%">
         <l-map
             :zoom="zoom"
             :center="center"
@@ -51,6 +71,18 @@ export default {
                 :maxZoom="maxZoom"
             >
             </l-tile-layer>
+            <l-feature-group ref="featureGroup">
+                <l-circle
+                    v-for="(circle, index) in circles"
+                    :key="index"
+                    :lat-lng="[circle.geometry.coordinates[1], circle.geometry.coordinates[0]]"
+                    :radius="4500"
+                    color="red"
+                >
+                </l-circle>
+
+            </l-feature-group>
+
         </l-map>
     </div>
 </template>
