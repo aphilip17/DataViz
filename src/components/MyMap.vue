a3ad5e466d559429c95a6cc854968895a5586102
aurore <aurore.philip@gmail.com>
2020-05-06 14:05:33 +0200

    Add geojson of depts + fetch depts

--- src/components/MyMap.vue
+++ src/components/MyMap.vue
@@ -3,21 +3,38 @@ import { latLng } from 'leaflet';
 import { LMap, LTileLayer } from 'vue2-leaflet';
 
 export default {
-  name: 'my-map',
-  components: {
-      LMap,
-      LTileLayer,
-  },
-  data() {
-    return {
-        zoom: 3,
-        center: latLng(47.41322, -1.219482),
-        url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 19
+    name: 'my-map',
+    components: {
+        LMap,
+        LTileLayer,
+    },
+    data() {
+        return {
+            zoom: 3,
+            center: latLng(47.41322, -1.219482),
+            url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            subdomains: 'abcd',
+            maxZoom: 19
+        }
+    },
+
+    created() {
+        this.getDeptsGeojson();
+    },
+
+    methods: {
+        async getDeptsGeojson() {
+            try {
+                const response = await fetch('http://localhost:8080/depts.json');
+                const depts = await response.json();
+                console.log(depts);
+            } catch (error) {
+                console.log(error);
+            }
+
+        }
     }
-  }
 }
 </script>
 
