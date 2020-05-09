ea2eed6c8044a7e8eb6e5d6cc19805890a54b0d2
aurore <aurore.philip@gmail.com>
2020-05-09 23:24:48 +0200

    Install materialize + install sass loader to be able to override sass variables

--- src/components/DataBar.vue
+++ src/components/DataBar.vue
@@ -6,14 +6,16 @@ export default {
 </script>
 
 <template>
-  <div class="data-bar"></div>
+  <div class="data-bar">
+    <a class="btn-large waves-effect waves-light">Get Started</a>
+    <nav></nav>
+  </div>
 </template>
 
 <style scoped>
   .data-bar {
     height: 150px;
     margin: 20px;
-    background-color: red;
     box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'leaflet/dist/leaflet.css'
+import './app.scss';
 
 Vue.config.productionTip = false
 
