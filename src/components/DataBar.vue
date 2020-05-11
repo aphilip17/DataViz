7e08ce7c2a4f6443eaf0efc13866b12f044b4d67
aurore <aurore.philip@gmail.com>
2020-05-11 12:09:52 +0200

    Add action on click, for each buttons

--- src/components/DataBar.vue
+++ src/components/DataBar.vue
@@ -2,13 +2,36 @@
 
 export default {
   name: 'data-bar',
+  data() {
+    return {
+      options: [{
+        disabled: true,
+        name: 'Hospitalized'
+      }, {
+        disabled: true,
+        name: 'Critical care'
+      }, {
+        disabled: true,
+        name: 'Healed'
+      }, {
+        disabled: false,
+        name: 'Deaths'
+      }]
+    }
+  },
 }
 </script>
 
 <template>
   <div class="data-bar">
-    <a class="btn-large waves-effect waves-light">Get Started</a>
-    <nav></nav>
+    <a
+      v-for="(opt, index) in options"
+      :key="index"
+      :class="{ 'disabled-data': opt.disabled, 'btn-large': true }"
+      @click="opt.disabled = !opt.disabled"
+    >
+      {{ opt.name }}
+    </a>
   </div>
 </template>
 
@@ -17,5 +40,13 @@ export default {
     height: 150px;
     margin: 20px;
     box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .disabled-data {
+    background-color: #DFDFDF !important;
+    color: #9F9F9F !important;
   }
 </style>
