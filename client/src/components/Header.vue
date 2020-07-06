44680ef0717b6d149f2b31abb412339e201af4f4
aurore <aurore.philip@gmail.com>
2020-07-06 20:02:50 +0200

    Create header vue but don't use it

---
+++ client/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'header-dash'
+}
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 35px;
+  background: linear-gradient(0.25turn, #a6ffcb, #ffa751, #f80759);
+}
+
+</style>
